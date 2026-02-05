1d26eb80665c1f2f9e3349fc6df240fb22d22b96
Clint Simiyu <clintsimiyu004@gmail.com>
2026-02-05 13:45:16 +0300

    Updated check_env.sh

--- check_env.sh
+++ check_env.sh
@@ -6,8 +6,10 @@ required_vars=(
     "MONGO_URL"
     "MODEL_URL"
     "CLIENT_URL"
-    "JWT_SECRET"
     "OPENAI_API_KEY"
+    "SUPABASE_URL"
+    "SUPABASE_SERVICE_ROLE_KEY"
+    "GOOGLE_API_KEY"
 )
 
 # Array to store missing variables
